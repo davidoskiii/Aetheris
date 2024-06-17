ff3f0d06e0162b40cf0e7917a49c9a02a5038b80
davidoskiii <davide.mascherin9@gmail.com>
2024-06-17 16:39:08 +0200

    Refactored keyboard input

--- main.c
+++ main.c
@@ -25,18 +25,37 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'): {
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      exit(0);
+      break;
+    }
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 int main() {
   enableRawMode();
 
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
+    editorRefreshScreen();
+    editorProcessKeypress();
   }
+
   return 0;
 }
