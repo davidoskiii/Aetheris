6ea63091fe95d0cb62e7becb7103c5bdec0ddb73
davidoskiii <davide.mascherin9@gmail.com>
2024-06-17 20:52:10 +0200

    Fixed a bug

--- utils/utils.c
+++ utils/utils.c
@@ -19,19 +19,14 @@ void die(const char *s) {
 void editorOpen(char *filename) {
   FILE *fp = fopen(filename, "r");
   if (!fp) die("fopen");
+
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
-  linelen = getline(&line, &linecap, fp);
-
   while ((linelen = getline(&line, &linecap, fp)) != -1) {
-    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
-      linelen--;
-    }
+    while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
     editorAppendRow(line, linelen);
   }
-
   free(line);
   fclose(fp);
 }
-
