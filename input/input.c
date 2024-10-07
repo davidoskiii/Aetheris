2428b2f35bc60c2e046b6dbda31515957cbd8401
davidoskiii <davide.mascherin9@gmail.com>
2024-10-07 16:35:45 +0200

    Fixed yet another bug with escape key

--- input/input.c
+++ input/input.c
@@ -724,6 +724,7 @@ void editorVisualProcessKeypress() {
     }
 
     case '\x1b': 
+      if (editor.cx != 0) editor.cx = editor.cx - 1;
       editor.mode = MODE_NORMAL;
       editor.is_selected = 0;
       break;
@@ -888,6 +889,7 @@ void editorVisualLineProcessKeypress() {
     }
 
     case '\x1b': 
+      if (editor.cx != 0) editor.cx = editor.cx - 1;
       editor.mode = MODE_NORMAL;
       editor.is_selected = 0;
       break;
